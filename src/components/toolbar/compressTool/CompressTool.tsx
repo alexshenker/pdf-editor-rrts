e967b212e9dd3a9aba4fe71bf2c8337b1822018f
aleksey shenker <alex@alekseys-MacBook-Pro.local>
2021-12-15 10:39:53 -0500

    changed resizeTool to compressTool

--- src/components/toolbar/compressTool/CompressTool.tsx
+++ src/components/toolbar/compressTool/CompressTool.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { RootState } from '../../../reducers'
-import styles from './ResizeTool.module.css'
+import styles from './CompressTool.module.css'
 
 //UI
 import ToolBtn from '../../../ui/ToolBtn'
@@ -15,8 +15,8 @@ Larger number = more compression / lower quality
 - compression options (1-10, 1-100, low-medium-high)
 */
 
-export default function ResizeTool() {
+export default function CompressTool() {
   const byteSize = useSelector((state: RootState) => state.file.byteSize)
 
-  return <div className={styles.resize_tool}></div>
+  return <div className={styles.compress_tool}></div>
 }
