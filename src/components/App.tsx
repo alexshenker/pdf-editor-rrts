29725ac459ce34944f7dd744cccf606d06a8049c
aleksey shenker <alex@alekseys-MacBook-Pro.local>
2021-12-17 21:55:22 -0500

    added preliminary leftpane component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
+import React from 'react'
 
+import styles from './App.module.css'
 //components
-import Toolbar from './toolbar/Toolbar';
-import Dropzone from './dropzone/Dropzone';
-import Viewer from './viewer/Viewer';
+import Toolbar from './toolbar/Toolbar'
+import Dropzone from './dropzone/Dropzone'
 
+import LeftPane from './leftpane/LeftPane'
+import Viewer from './viewer/Viewer'
 
 function App() {
   return (
     <div>
       <Dropzone />
-      <Toolbar />
-      <Viewer />
+      <div className={styles.app_body}>
+        <div>
+          <LeftPane />
+        </div>
+        <div>
+          <Toolbar />
+          <Viewer />
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/leftpane/LeftPane.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+import styles from './LeftPane.module.css'
+
+export default function LeftPane() {
+  return <div className={styles.leftpane}></div>
+}
