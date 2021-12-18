d9366932996279ead941f9b46047822703eebe63
aleksey shenker <alex@alekseys-MacBook-Pro.local>
2021-12-18 13:21:55 -0500

    added children to leftpane

--- src/components/leftpane/LeftPane.tsx
+++ src/components/leftpane/LeftPane.tsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { FC } from 'react'
 
 import styles from './LeftPane.module.css'
 
-export default function LeftPane() {
-  return <div className={styles.leftpane}></div>
+const LeftPane: FC = ({ children }) => {
+  return <div className={styles.leftpane}>{children}</div>
 }
+
+export default LeftPane
